8504010ae5d57ea9b51801e2f1dca48e67450117
HUANG <starnightsola@gmail.com>
2025-06-02 15:22:43 +0900

    Header.test.tsxエラー修正

--- src/components/Header.test.tsx
+++ src/components/Header.test.tsx
@@ -4,6 +4,13 @@ import Header from './Header';
 
 describe('Header', () => {
   it('renders correctly', () => {
-    render(<Header onMenuClick={() => {}} isDrawerOpen={false} />);
+    render(
+      <Header
+        onMenuClick={() => {}}
+        isDrawerOpen={false}
+        mode="light" // もしくは "dark"
+        onToggleTheme={() => {}}
+      />
+    );
   });
 });
